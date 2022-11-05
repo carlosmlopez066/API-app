58913af33c04b563782d10830efbb5bb05f00e07
Carlos Lopez <carlosmlopez066@gmail.com>
2022-11-05 12:05:46 -0400

    Agregando nodes y archivo de routes

---
+++ src/navegation.js
@@ -0,0 +1,116 @@
+searchFormBtn.addEventListener('click', () => {
+    location.hash = '#search='
+});
+trendingBtn.addEventListener('click', () => {
+    location.hash = '#trends='
+});
+arrowBtn.addEventListener('click', () => {
+    location.hash = '#home'
+});
+
+window.addEventListener('DOMContentLoaded', navegator, false);
+window.addEventListener('hashchange', navegator, false);
+function navegator() {
+    console.log({ location });
+
+    if (location.hash.startsWith('#trends')) {
+        trendsPage();
+    } else if (location.hash.startsWith('#search=')) {
+        SearchPage();
+    } else if (location.hash.startsWith('#movie=')) {
+        movieDetailsPage();
+    } else if (location.hash.startsWith('#category=')) {
+        categoryPage();
+    } else {
+        homePage();
+    }
+}
+
+function trendsPage() {
+    console.log('TRENDS')
+    headerSection.classList.remove('header-container--long');
+    headerSection.style.background = '';
+    arrowBtn.classList.remove('inactive');
+    arrowBtn.classList.remove('header-arrow--white');
+    headerCategoryTitle.classList.remove('inactive');
+    headerTitle.classList.add('inactive');
+    searchForm.classList.remove('inactive');
+
+    trendingPreviewSection.classList.add('inactive');
+    categoriesPreviewSection.classList.add('inactive');
+    genericSection.classList.remove('inactive');
+    movieDetailSection.classList.add('inactive');
+}
+function SearchPage() {
+    console.log('SEARCH')
+
+    headerSection.classList.remove('header-container--long');
+    headerSection.style.background = '';
+    arrowBtn.classList.remove('inactive');
+    arrowBtn.classList.remove('header-arrow--white');
+    headerCategoryTitle.classList.remove('inactive');
+    headerTitle.classList.add('inactive');
+    searchForm.classList.add('inactive');
+
+    trendingPreviewSection.classList.add('inactive');
+    categoriesPreviewSection.classList.add('inactive');
+    genericSection.classList.remove('inactive');
+    movieDetailSection.classList.add('inactive');
+}
+function movieDetailsPage() {
+    console.log('Movie')
+
+    headerSection.classList.add('header-container--long');
+    // headerSection.style.background = '';
+    arrowBtn.classList.remove('inactive');
+    arrowBtn.classList.add('header-arrow--white');
+    headerCategoryTitle.classList.add('inactive');
+    headerTitle.classList.add('inactive');
+    searchForm.classList.add('inactive');
+
+    trendingPreviewSection.classList.add('inactive');
+    categoriesPreviewSection.classList.add('inactive');
+    genericSection.classList.add('inactive');
+    movieDetailSection.classList.remove('inactive');
+}
+function categoryPage() {
+    console.log('Cateories')
+
+    headerSection.classList.remove('header-container--long');
+    headerSection.style.background = '';
+    arrowBtn.classList.remove('inactive');
+    arrowBtn.classList.remove('header-arrow--white');
+    headerCategoryTitle.classList.remove('inactive');
+    headerTitle.classList.add('inactive');
+    searchForm.classList.add('inactive');
+
+    trendingPreviewSection.classList.add('inactive');
+    categoriesPreviewSection.classList.add('inactive');
+    genericSection.classList.remove('inactive');
+    movieDetailSection.classList.add('inactive');
+
+    const [_, categoryData] = location.hash.split('=') //esto nos devuelve un array ['#category','id-name']
+    const [categoryId, categoryName] = categoryData.split('-')
+
+    getMoviesByCategories(categoryId);
+    headerCategoryTitle.innerHTML = categoryName;
+}
+function homePage() {
+    console.log('Home');
+
+    headerSection.classList.remove('header-container--long');
+    headerSection.style.background = '';
+    arrowBtn.classList.add('inactive');
+    arrowBtn.classList.remove('header-arrow--white');
+    headerCategoryTitle.classList.add('inactive');
+    headerTitle.classList.remove('inactive');
+    searchForm.classList.remove('inactive');
+
+    trendingPreviewSection.classList.remove('inactive');
+    categoriesPreviewSection.classList.remove('inactive');
+    genericSection.classList.add('inactive');
+    movieDetailSection.classList.add('inactive');
+
+    getTrendingMoviesPreview()
+    getCategoriesPreview()
+}
